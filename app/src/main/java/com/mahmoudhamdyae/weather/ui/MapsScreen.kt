351941622b5972c147fea2d91d9597aef8ccdb2a
mahmoudhamdyae <mahmoudhamdyae@gmail.com>
2023-05-19 11:46:47 +0300

    Map Marker Fix

--- app/src/main/java/com/mahmoudhamdyae/weather/ui/MapsScreen.kt
+++ app/src/main/java/com/mahmoudhamdyae/weather/ui/MapsScreen.kt
@@ -3,6 +3,10 @@ package com.mahmoudhamdyae.weather.ui
 import android.widget.Toast
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import com.google.android.gms.maps.model.CameraPosition
@@ -21,11 +25,10 @@ fun MapsScreen(
 ) {
 
     val context = LocalContext.current
-    val latitude = uiState.latitude
-    val longitude = uiState.longitude
 
-    var myLocation =
-        LatLng(latitude ?: 31.045162, longitude ?: 31.399642)
+    var myLocation by rememberSaveable {
+        mutableStateOf(LatLng(uiState.latitude ?: 31.045162, uiState.longitude ?: 31.399642))
+    }
     val cameraPositionState = rememberCameraPositionState {
         position = CameraPosition.fromLatLngZoom(myLocation, 1f)
     }
@@ -39,7 +42,7 @@ fun MapsScreen(
             Toast.makeText(context, "Location Updated", Toast.LENGTH_SHORT).show()
         }
     ) {
-        if (latitude != null) {
+        if (myLocation.latitude != 31.045162) {
             Marker(
                 state = MarkerState(position = myLocation),
                 title = "My Last Location",
